d7ab11a5ceb9f13a170226773c5561b6236a0f04
mook <615021000315@mail.rmutk.ac.th>
2021-02-24 09:53:55 +0700

    1

---
+++ helpers/Menu.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+	public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-align-justify "></i>'
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'post', 
+			'label' => 'Post', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+
+	
+	
+	public static $name = array();
+
+}
